6e9761710b08ed86283d66d7c96242160af5ebfe
Leonardo <yleeoembd@gmail.com>
2022-07-29 12:55:12 -0300

    feat trocando valores do portifolio

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "samuelleao",
-  whatsappNumber: "+5541111111111",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Leonardo Henrique",
+  githubUser: "Leeo-Henrique",
+  linkedinUser: "leonardo-henrique-08396922a",
+  whatsappNumber: "+5531973008108",
+  emailUser: "leeo.contatopessoal@gmail.com",
+  instagramUser: "leeo_henrique.17",
   facebookUser: "",
 };
